1e630f1bddae814ff4b2d2b8269633b416e47092
bleujin <bleujin@gmail.com>
2013-04-29 01:00:04 +0900

    modify PathBy testCase

--- test/net/ion/craken/node/ref/TestPathBy.java
+++ test/net/ion/craken/node/ref/TestPathBy.java
@@ -1,5 +1,7 @@
 package net.ion.craken.node.ref;
 
+import net.ion.craken.node.TransactionJob;
+import net.ion.craken.node.WriteSession;
 import net.ion.craken.node.crud.TestBaseCrud;
 import net.ion.framework.util.Debug;
 
@@ -9,8 +11,23 @@ public class TestPathBy extends TestBaseCrud {
 	public void testNotFoundPath() throws Exception {
 		
 		assertEquals(true, ! session.exists("/bleujin")) ;
-		assertEquals(true, session.pathBy("/bleujin") != null) ; // not null 
 		
-		assertEquals(0, session.pathBy("/bleujin").keys().size()) ;
+		try {
+			assertEquals(true, session.pathBy("/bleujin") != null) ;
+			fail() ;
+		} catch(IllegalArgumentException expect){}
+		
+	}
+	
+	public void testInWriteSession() throws Exception {
+		assertEquals(true, ! session.exists("/bleujin")) ;
+		session.tranSync(new TransactionJob<Void>() {
+			@Override
+			public Void handle(WriteSession wsession) {
+				assertEquals(true, wsession.pathBy("/bleujin") != null) ;
+				return null;
+			}
+		}) ;
+		
 	}
 }
